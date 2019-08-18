baff73ae00a576b42fc1ccafcdc51b8c8d966b44
yaegashi-shizuku <54224977+yaegashi-shizuku@users.noreply.github.com>
2019-08-18 11:57:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -31,5 +31,4 @@ def home():
         f = (category, length)
         fill.append(f)
     return render_template("index.html", cat=fill)
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port="3000")
+
